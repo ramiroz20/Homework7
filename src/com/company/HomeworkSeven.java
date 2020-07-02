226189db0ec5562fa57d8affc439fdf9994a1bae
Roman <romantuser69@gmail.com>
2020-07-02 20:47:16 +0300

    Метод выводит в консоль сообщение заданное

---
+++ src/com/company/HomeworkSeven.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class HomeworkSeven {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Bye world...");
+    }
+}
