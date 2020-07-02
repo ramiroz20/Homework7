af317a66d2ea670d0e1940be723e380db7026fd2
Roman <66552401+ramiroz20@users.noreply.github.com>
2020-07-02 20:56:18 +0300

    Add files via upload

---
+++ Homework7/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        String txtToPrint = "Roman";
+        // выводим строку txtToPrint в консоль
+        System.out.println("Привет " + txtToPrint + " !");
+    }
+}
+
+
+
